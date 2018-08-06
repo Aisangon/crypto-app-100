53008e46de32d48e789a8785f504b0af206488ca
Aisangon <ainelainel@hotmail.com>
2018-08-06 21:49:13 +0200

    add sort functionality, routing, bitcoin table

---
+++ cryptoapp/src/not-found/not-found.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'not-found',
+    templateUrl: './not-found.component.html',
+    styleUrls: ['./not-found.component.css']
+})
+export class NotFoundComponent {}
